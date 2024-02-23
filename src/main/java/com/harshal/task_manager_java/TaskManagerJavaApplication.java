e450a3f89162c10235fddc1cffc39508ad83fd96
hnk2001 <hasrhalkotkar1409@gmail.com>
2024-02-23 17:10:13 +0530

    initial setup of project via Spring Initializr
    Signed-off-by: hnk2001 <hasrhalkotkar1409@gmail.com>

---
+++ src/main/java/com/harshal/task_manager_java/TaskManagerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.harshal.task_manager_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harshal/task_manager_java/TaskManagerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshal.task_manager_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
